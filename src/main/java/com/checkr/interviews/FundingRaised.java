8952293b3f3d78f1b44c34701be88762c2dbcf03
EgorAnti <egor.anti.00@mail.ru>
2025-04-01 23:23:36 +0300

    Delete main method from FundingRaised

--- src/main/java/com/checkr/interviews/FundingRaised.java
+++ src/main/java/com/checkr/interviews/FundingRaised.java
@@ -60,18 +60,5 @@ public class FundingRaised {
 
         throw new NoSuchEntryException();
     }
-
-    public static void main(String[] args) {
-        try {
-            Map<String, String> options = new HashMap<> ();
-            options.put("company_name", "Facebook");
-            options.put("round", "a");
-            System.out.print(FundingRaised.GetAllRowsByOptions(options).size());
-
-        } catch(IOException e) {
-            System.out.print(e.getMessage());
-            System.out.print("error");
-        }
-    }
 }
 
--- src/main/java/com/checkr/interviews/services/FindAllService.java
+++ src/main/java/com/checkr/interviews/services/FindAllService.java
@@ -22,6 +22,4 @@ public class FindAllService {
         }
         return results;
     }
-
-
 }
